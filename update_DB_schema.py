84cbc358bf610a363eb3c4956fd0bf74dcd819ab
really-lilly <tatkalillian@gmail.com>
2022-08-29 09:06:58 -0700

    add metadata and change schema

--- update_DB_schema.py
+++ update_DB_schema.py
@@ -5,25 +5,60 @@ astr = "mongodb+srv://data:VuRWQ@networks.wqx1t.mongodb.net"
 client = MongoClient(astr)
 database_names = client.list_database_names()
 print(database_names)
-# db = client['networks']
-db = client.networks
-collection = db['networks']
+org = client.networks
+og = org['networks']
+db = client.models
+collection = db['models']
 cur = collection.find({})
 
-# modelTypes = ["oscillator", "random"]
-#
-#
-#
-#
-# currentDB = mm.query_database({})
-#
-# models = []
-#
-# min_reactions = 1000
-# min_species = 1000
-# max_reactions = 0
-# max_species = 0
-#
+
+
+
+
+
+for i, model in enumerate(cur):
+    if i % 100 == 0:
+        print(f"working on model {i}...")
+    ID = model["ID"]
+    oldModel = og.find({"ID": ID})
+    newVal = {}
+    try:
+        newVal['autocatalysisPresent'] = oldModel['Autocatalysis Present']
+    except:
+        newVal['autocatalysisPresent'] = None
+    try:
+        newVal['degradationPresent'] = oldModel['Degradation Present']
+    except:
+        newVal['degradationPresent'] = None
+    try:
+        newVal['initialProbabilities'] = oldModel['initialProbabilities']
+    except:
+        newVal['initialProbabilities'] = None
+    try:
+        newVal['addReactionProbabilities'] = oldModel['addReactionProbabilities']
+    except:
+        newVal['addReactionProbabilities'] = None
+
+    # try:
+    #     newVal["combinedReactions"] = oldModel["combinedReactions"]
+    # except:
+    #     newVal["combinedReactions"] = None
+    #
+    # try:
+    #     newVal['deletedReactions'] = oldModel['deletedReactions']
+    # except:
+    #     newVal['deletedReactions'] = None
+    #
+    # try:
+    #     newVal['reactionCounts'] = oldModel['reactionCounts']
+    # except:
+    #     newVal['reactionCounts'] = None
+
+    collection.update_one({"ID": ID}, {"$set": newVal})
+
+
+
+
 # for i, model in enumerate(currentDB):
 #     if i%100 == 0:
 #         print(i)
@@ -69,17 +104,19 @@ cur = collection.find({})
 #
 #     models.append(entry)
 
-networkDB = client['meta_data']
-collection = db['meta_data']
-
-entry = {'totalModels': 10127,
-         'totalOscillators': 2065,
-         'totalRandom': 8062,
-         'modelTypes': ["oscillator", "random"]}
-
-collection.insert_one(entry)
+# networkDB = client['meta_data']
+# collection = db['meta_data']
+#
+# entry = {'totalModels': 18189,
+#          'totalOscillators': 2065,
+#          'totalRandom': 16124,
+#          'modelTypes': ["oscillator", "random"]}
+#
+# collection.insert_one(entry)
 # for entry in models:
 #     collection.insert_one(entry)
 #
 #
 # r = collection.find({})
+
+### Un-nest the antimony dictionary
